b9d0b05b4fc88ae653e824bd0dbdce4761b169cb
tarabore <tarabore@gmail.com>
2021-12-10 21:46:16 -0300

    Primer proyecto colores Profe

---
+++ src/coloresprofe/ColoresProfe.java
@@ -0,0 +1,17 @@
+package coloresprofe;
+
+public class ColoresProfe {
+
+    public static void main(String[] args) {
+        String color1, color2, color3, color4;
+        color1 = "AZUL";
+        color2 = "ROJO";
+        color3 = "VIOLETA";
+        color4 = "TORNASOL";
+        System.out.printf("%s es mi primer color favorito\n", color1);
+        System.out.printf("%s es mi segundo color favorito\n", color2);
+        System.out.printf("%s es mi tercer color favorito\n", color3);
+        System.out.printf("%s es mi tercer color favorito\n", color4);
+        
+    }
+}
