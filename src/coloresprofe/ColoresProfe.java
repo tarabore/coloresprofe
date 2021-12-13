f6d3ce4d5862960d031ab3d2009f1f761d1c608b
joel carbonetti <carbonettijoel@gmail.com>
2021-12-12 22:30:06 -0300

    Cambio de colores favoritos

--- src/coloresprofe/ColoresProfe.java
+++ src/coloresprofe/ColoresProfe.java
@@ -4,14 +4,14 @@ public class ColoresProfe {
 
     public static void main(String[] args) {
         String color1, color2, color3, color4;
-        color1 = "AZUL";
+        color1 = "VERDE";
         color2 = "ROJO";
-        color3 = "VIOLETA";
+        color3 = "PURPURA";
         color4 = "TORNASOL";
         System.out.printf("%s es mi primer color favorito\n", color1);
         System.out.printf("%s es mi segundo color favorito\n", color2);
         System.out.printf("%s es mi tercer color favorito\n", color3);
-        System.out.printf("%s es mi tercer color favorito\n", color4);
+        System.out.printf("%s es mi cuarto color favorito\n", color4);
         
     }
 }
